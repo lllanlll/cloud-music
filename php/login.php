58d6972eb08a04dca38919e2748e9638bcdc0da6
linjh8292 <linjh8292@fxiaoke.com>
2019-10-17 11:08:10 +0800

    feat:初始化

---
+++ php/login.php
@@ -0,0 +1,29 @@
+<?php
+    header("Content-type:text/html;charset=utf-8;");
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $conn = new mysqli('localhost','root','', 'cloudmusic');
+    if ($conn->connect_error){
+        echo '数据库连接失败！';
+        exit(0);
+    }else{
+        if ($username == ''){
+            echo '<script>alert("请输入用户名！");history.go(-1);</script>';
+            exit(0);
+        }
+        if ($password == ''){
+            echo '<script>alert("请输入密码！");history.go(-1);</script>';
+            exit(0);
+        }
+        $sql = "select username,password from userinfo where username = '$_POST[username]' and password = '$_POST[password]'";
+        $result = $conn->query($sql);
+        $number = mysqli_num_rows($result);
+        if ($number) {
+            header("Location:http://localhost/cloud-music/pages/main.html?username=$username");
+            echo '<script>window.location="../pages/main.html";</script>';
+        } else {
+            echo '<script>alert("用户名或密码错误！");history.go(-1);</script>';
+        }
+    }
+    
+?>
---
+++ php/register.php
@@ -0,0 +1,55 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+$username = $_POST['username'];
+$password = $_POST['password'];
+$repassword = $_POST['repassword'];
+$telphone = $_POST['telphone'];
+if ($username == '') {
+  echo '<script>alert("请输入用户名!");history.go(-1);</script>';
+  exit(0);
+}
+if ($username.length > 20) {
+  echo '<script>alert("用户名要 < 20位!");history.go(-1);</script>';
+  exit(0);
+}
+if ($password == '') {
+  echo '<script>alert("请输入密码");history.go(-1);</script>';
+  exit(0);
+}
+if (strlen($password) >= 20 || strlen($password) <= 6) {
+  echo '<script>alert("密码要在6-20位");history.go(-1);</script>';
+  exit(0);
+}
+if ($password != $repassword) {
+  echo '<script>alert("密码与确认密码应该一致");history.go(-1);</script>';
+  exit(0);
+}
+if ($password == $repassword) {
+  $conn = new mysqli('localhost', 'root', '', 'cloudmusic');
+  if ($conn->connect_error) {
+    echo '数据库连接失败！';
+    exit(0);
+  } else {
+    $sqluser = "select username from userinfo where username = '$_POST[username]'";
+    $sqlphone = "select telphone from userinfo where telphone = '$_POST[telphone]'";
+    $resultuser = $conn->query($sqluser);
+    $resultphone = $conn->query($sqlphone);
+    $numberuser = mysqli_num_rows($resultuser);
+    $numberphone = mysqli_num_rows($resultphone);
+    if ($numberuser) {
+      echo '<script>alert("用户名已经存在");history.go(-1);</script>';
+    } else if($numberphone) {
+      echo '<script>alert("手机号已经被注册");history.go(-1);</script>';
+    } else {
+      $sql_insert = "insert into userinfo (username,password,telphone) values('$_POST[username]','$_POST[password]','$_POST[telphone]')";
+      $res_insert = $conn->query($sql_insert);
+      if ($res_insert) {
+        echo '<script>window.location="../pages/login.html";</script>';
+      } else {
+        echo "<script>alert('系统繁忙，请稍候！');</script>";
+      }
+    }
+  }
+} else {
+  echo "<script>alert('提交未成功！'); history.go(-1);</script>";
+}
